aa82c6d0a4c99e66e50a1c054b3828494d2a0424
Fay Carsons <faycarsons23@gmail.com>
2023-01-17 13:33:42 -0500

    added unit test + fixed errors parsing constants

--- src/symbolic_differentiator/core.clj
+++ src/symbolic_differentiator/core.clj
@@ -1,5 +1,7 @@
 (ns symbolic-differentiator.core
-  (:require [clojure.string :as s]))
+  (:use clojure.test)
+  (:require [clojure.string :as s]
+            [clojure.test :as t]))
 
 (use 'symbolic-differentiator.core :reload)
 
@@ -21,40 +23,47 @@
 (def get-factor #(re-find #"^-?[0-9]+" %))
 (def get-exponent #(re-find #"-?[0-9]+$" %))
 
-; takes monomial string and returns factor and/or exponent if present, nil otherwise
+; takes monomial string and returns factor and/or exponent if present
+; nil otherwise
 ; discards constants 
 (defn parse-monomial [monomial]
-  (when (> (count monomial) 1)
-    (let [factor (if-let [num (get-factor monomial)]
-                   (read-string num)
-                   1)
-          exponent (if-let [num (get-exponent monomial)]
-                     (read-string num)
-                     1)]
-      [(* factor exponent) (dec exponent)])))
+  (when (> (count monomial) 1)  
+   (let [factor (if-let [num (get-factor monomial)]
+                  (read-string num)
+                  1)
+         exponent (if-let [num (get-exponent monomial)]
+                    (read-string num)
+                    1)]
+     [(* factor exponent) (dec exponent)])))
 
 ; takes in raw unformatted polynomial string, splits into vector of strings, 
-; parses those strings into a vector of two element vectors, does differentiation,
-; and converts back to formatted string
-(defn deriv [expr]
-  (let [monomials (-> expr
+; parses strings into vector of factor + exponent, does differentiation,
+; converts back to formatted string
+(defn differeniate [expr]
+  (let [monomials (-> expr 
+                      (format)
                       (s/replace #"[-]" "+-")
                       (s/split #"[+]"))
         var (re-matches #"[a-z]" expr)]
     (->> monomials
          (map #(s/replace % #" " ""))
+         (filter #(not (parse-long %)))
          (map parse-monomial)
          (stringify-pairs))))
 
-(defn main []
-  (let [polynomial-string "-12x^3 + x^2 - 5x + 6"
-        polynomial-string-two "5x2+ 2x+7"
-        derivative (deriv polynomial-string)
-        derivative-two (deriv polynomial-string-two)]
-    (println derivative)
-    (println derivative-two)))
+; testing
+(deftest symbolic-differentiator
+ (let [poly-vec ["-12x^3 + x^2 - 5x + 6"
+                 "5x2+ 2x+7"
+                 "6x^4 + 3x^3 + 3x^2 + 2x + 1"
+                 "x^8 + x^5 + 3x + 10"]
+       derivatives (map differeniate poly-vec)
+       test-vec ["-36x^2 + 2x - 5"
+                 "10x + 2"
+                 "24x^3 + 9x^2 + 6x + 2"
+                 "8x^7 + 5x^4 + 3"]]
+   (dotimes [n (count derivatives)]
+     (is (= (nth derivatives n)
+            (nth test-vec n))))))
 
-(main)
-
-
-
+(run-tests 'symbolic-differentiator.core)
