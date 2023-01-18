3d675b35134ab22d7fd245c8ace79704bd03fd2c
Fay Carsons <faycarsons23@gmail.com>
2023-01-17 19:38:24 -0500

    improved handling of constants, cleaned up, step towards implementing higher order derivatives

--- src/symbolic_differentiator/core.clj
+++ src/symbolic_differentiator/core.clj
@@ -5,6 +5,10 @@
 
 (use 'symbolic-differentiator.core :reload)
 
+(defn log [v]
+  (println v)
+  v)
+
 (def parse-pair #(cond
                    (== (second %) 0) (str (first %))
                    (== (second %) 1) (str (first %) "x")
@@ -23,32 +27,38 @@
 (def get-factor #(re-find #"^-?[0-9]+" %))
 (def get-exponent #(re-find #"-?[0-9]+$" %))
 
+(defn compute-derivative [[factor exponent]]
+  [(* factor exponent) (dec exponent)])
+
 ; takes monomial string and returns factor and/or exponent if present
 ; nil otherwise
 ; discards constants 
 (defn parse-monomial [monomial]
-  (when (> (count monomial) 1)  
+  (if-let [contant (parse-long monomial)]
+    [contant 0]
    (let [factor (if-let [num (get-factor monomial)]
-                  (read-string num)
+                  (parse-long num)
                   1)
          exponent (if-let [num (get-exponent monomial)]
-                    (read-string num)
+                    (parse-long num)
                     1)]
-     [(* factor exponent) (dec exponent)])))
+     [factor exponent])))
+
+
 
 ; takes in raw unformatted polynomial string, splits into vector of strings, 
 ; parses strings into vector of factor + exponent, does differentiation,
 ; converts back to formatted string
-(defn differeniate [expr]
+(defn differentiate [expr]
   (let [monomials (-> expr 
-                      (format)
                       (s/replace #"[-]" "+-")
-                      (s/split #"[+]"))
-        var (re-matches #"[a-z]" expr)]
+                      (s/split #"[+]"))]
     (->> monomials
          (map #(s/replace % #" " ""))
-         (filter #(not (parse-long %)))
+         (remove #(or (parse-long %)
+                      (s/blank? %)))
          (map parse-monomial)
+         (map compute-derivative)
          (stringify-pairs))))
 
 ; testing
@@ -56,12 +66,18 @@
  (let [poly-vec ["-12x^3 + x^2 - 5x + 6"
                  "5x2+ 2x+7"
                  "6x^4 + 3x^3 + 3x^2 + 2x + 1"
-                 "x^8 + x^5 + 3x + 10"]
-       derivatives (map differeniate poly-vec)
+                 "x^8 + x^5 + 3x + 10"
+                 "5x - x^2 - 3x^7 - 10"
+                 "12x^7 - 10x - 2x^3 - 1 - 23x^100"
+                 "5 + 3 + 1 + 10 + 10000000 + 2x^3 - 3x^29 - 100 + 420x"]
+       derivatives (map differentiate poly-vec)
        test-vec ["-36x^2 + 2x - 5"
                  "10x + 2"
                  "24x^3 + 9x^2 + 6x + 2"
-                 "8x^7 + 5x^4 + 3"]]
+                 "8x^7 + 5x^4 + 3"
+                 "5 + 2x - 21x^6"
+                 "84x^6 - 10 - 6x^2 - 2300x^99"
+                 "6x^2 - 87x^28 + 420"]]
    (dotimes [n (count derivatives)]
      (is (= (nth derivatives n)
             (nth test-vec n))))))
