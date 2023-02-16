d11b5e4ce2afc1467715cc30b8a6ebdaf04baa1e
Fay Carsons <faycarsons23@gmail.com>
2023-02-15 22:28:11 -0500

    fixing commit error

--- src/symbolic_differentiator/core.clj
+++ src/symbolic_differentiator/core.clj
@@ -68,28 +68,17 @@
 
 (do
   (deftest symbolic-differentiator
-    (let [poly-vec ["2x^3 + 5"
-                    "-12x^3 + x^2 - 5x + 6"
-                    "5x2+ 2x+7"
-                    "6x^4 + 3x^3 + 3x^2 + 2x + 1"
-                    "x^8 + x^5 + 3x + 10"
-                    "5x - x^2 - 3x^7 - 10"
-                    "12x^7 - 10x - 2x^3 - 1 - 23x^100"
-                    "5 + 3 + 1 + 10 + 10000000 + 2x^3 - 3x^29 - 100 + 420x"]
-          
-          derivatives (map differentiate poly-vec)
-          
-          test-vec ["6x^2"
-                    "-36x^2 + 2x - 5"
-                    "10x + 2"
-                    "24x^3 + 9x^2 + 6x + 2"
-                    "8x^7 + 5x^4 + 3"
-                    "5 + 2x - 21x^6"
-                    "84x^6 - 10 - 6x^2 - 2300x^99"
-                    "6x^2 - 87x^28 + 420"]]
-      
+    (let [poly-vec ["-4x^2 - 4x^2 - 2x^4 - 6x^2 - 2x + 7x"
+                    "6x^4 - 3x^3 + 8x^4 + 4x^4 - 2x^2 - 7x^4"
+                    "1x^2 + 3x^3 + 3x^4"
+                    "4x^4 + 7x - 7x - 4x^3 - 5x^2"]
+          derivatives (map differentiate poly-vec) 
+          test-vec ["8x^3 - 28x + 5"
+                    "44x^3 - 9x^2 - 4x"
+                    "12x^3 + 9x^2 + 2x"
+                    "16x^3 - 12x^2 - 10x"]] 
       (dotimes [n (count derivatives)]
         (is (= (nth derivatives n)
-               (nth test-vec n))))))
+               (nth test-vec n)))))) 
 
   (run-tests 'symbolic-differentiator.core))
